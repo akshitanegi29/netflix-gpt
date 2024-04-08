2d74079ce8902821198dabbf946213e90d2f2761
akshitanegi29 <akshita29negi@gmail.com>
2024-04-08 22:05:42 +0530

    CONSTANT

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const TMDBHeader = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTQ0NmQyMmZiZTk3ZGZjMGQ2NDU5M2Y4NGU0NmIyNSIsInN1YiI6IjY2MTE5ODZkMWZhMWM4MDE2M2QxY2I1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EKD0rYM_Fg4k7A2fAWb2B73BhqDKsVIsO9TLu7xJaDc",
+      "Bearer "+process.env.REACT_APP_TMBD_KEY
   },
 };
 
