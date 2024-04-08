d3a57c7309d8b2fb58058f046f2d27150661b79d
akshitanegi29 <akshita29negi@gmail.com>
2024-04-08 22:58:05 +0530

    shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="p-4 m-2 bg-black text-white bg-opacity-70">
+    <div className="p-4 m-2 bg-black text-white bg-opacity-70 h-screen">
       <div className="flex overflow-x-scroll">
         <div className="flex">
           <div className="w-36 m-3 h-64 md:w-48 pr-4 bg-slate-600 dark:bg-gray-700 max-w-sm animate-pulse"></div>
